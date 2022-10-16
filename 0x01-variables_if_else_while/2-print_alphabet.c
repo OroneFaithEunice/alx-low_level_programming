36d199afc8c936e8e881c42477cd9c7d398bdbac
OroneFaithEunice <oronefaith.of@gmail.com>
2022-10-16 07:26:18 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point prints alphabet in lowercase\n
+ * Return: 0 when success
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
