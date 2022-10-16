53bab959eeef59656db8c0d3c0544737b5d06bdc
OroneFaithEunice <oronefaith.of@gmail.com>
2022-10-16 07:50:14 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * main - Entry point prints lowercase alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
