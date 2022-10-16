4bf3b128f338dad611d1b6e69b54baae33ad05da
OroneFaithEunice <oronefaith.of@gmail.com>
2022-10-16 02:46:10 -0700

    created a puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- Serves as the entry point for the program
+ *
+ * Return: Returns zero by default if there are no errors otherwise returns non zero valve
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
