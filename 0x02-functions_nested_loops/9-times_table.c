fa88b23b406b3f34c51a6dce156c138838b0a7f1
OroneFaithEunice <oronefaith.of@gmail.com>
2022-12-11 04:28:18 -0800

    fixing betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * times_table - prints thetable 9 times
+ * time_table - prints thetable 9 times
  * rone = row, cone = column, d = digits of current result
- * Return: times table
+ * Return: time table
  * add extra space past single digit
  */
 
-void times_table(void)
+void time_table(void)
 {
 
 	int rone, cone, d;
